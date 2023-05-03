c876dd56b1a07ce9c7a8f108b6ca9305ae87fd0d
Tillman Pugh <Till.pugh@gmail.com>
2023-05-03 14:35:45 +1200

    first commit

---
+++ ninja.js
@@ -0,0 +1,24 @@
+class Ninja {
+    constructor(name, health) {
+        this.speed = 3;
+        this.strength = 3;
+        this.name = name;
+        this.health = health;
+    }
+    sayName() {
+        console.log(`The ninja's name is ${this.name}`)
+    }
+    showStats() {
+        console.log(`The ninja's name is ${this.name}. 
+        The ninja's strength is ${this.strength}. 
+        The ninja's speed is ${this.speed}. 
+        The ninja's health is ${this.health}. `)
+    }
+    drinkSake() {
+        this.health += 10;
+    }
+}
+const ninja1 = new Ninja("Tanjiro", 100);
+console.log(ninja1)
+
+
